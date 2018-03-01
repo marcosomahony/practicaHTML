8a8bc6da7d90bd11b4698ab050005d44cce6c59c
Catalin Beniamin Santa <catalin.beniamin@babel.es>
2018-03-01 14:02:14 +0100

    form.js to about (sigue sin funcionar)

--- scripts/main.js
+++ scripts/main.js
@@ -26,51 +26,6 @@ export class Main {
 
         this.cargarTemplate('home');
 
-        /*FORMULARIO */
-/*         this.formContacto = {
-            oDatos: {},
-            oParrafo: document.getElementById("parrafo_resultado"),
-
-            escribirDatos: function () {
-                document.getElementById("form_contacto").classList.toggle("oculto")
-                document.getElementById("resultado_sugerencia").classList.toggle("oculto")
-
-                for (var i in this.oDatos) {
-                    localStorage.setItem(i.toString(), this.oDatos[i])
-                    this.oParrafo.innerHTML += "<li>" + i + ": <strong>" +
-                        this.oDatos[i] + "</strong></li>";
-                };
-            },
-            getTextos: function () {
-                var inputs = document.querySelectorAll("input[type='text']")
-                for (var i = 0; i < inputs.length; i++) {
-                    this.oDatos[inputs[i].name] = inputs[i].value;
-                }
-                this.oDatos.Sugerencias = document.getElementById("Sugerencias").value;
-            },
-            validarFormulario: function () {
-                let nombre = document.getElementById("Nombre").value
-                let email = document.getElementById("Email").value
-
-                if (nombre == null || nombre == "") {
-                    alert("Inserte el nombre")          
-                    return false
-                }
-                if (email == null || email == "") {
-                    alert("Inserte el email")
-                    return false
-                }
-                return true
-            },
-            recogerDatos: function () {
-                this.getTextos();
-                if (this.validarFormulario()) {
-                    this.escribirDatos()
-                }
-            },
-        }; */
-/*         document.getElementById("submit").onclick = this.formContacto.recogerDatos.bind(this.formContacto)
- */
         /*MENU REPLEGABLE */
         document.addEventListener("click", function menuReplegable(){
             let opcionesRep = document.querySelector("#opRep").classList.toggle("esconder")
@@ -104,11 +59,6 @@ export class Main {
         // el HTML del elemnto se añade en el punto adecuado
         this.highlightSelected(id)
         this.vista.eMain.innerHTML = ELEM.innerHTML;
-
-        if(id=='about'){
-            console.log("Jejeejejfrewafeafsdafsdfsdafdasfdfdas");
-            () => new FormContacto();
-        }
     }
 
     highlightSelected(id) {
